8f705bc91ef5b7ada6a109d5243cdc1411c5c6b4
Jordan Monfort <16010052@e105m10.istic.univ-rennes1.fr>
2017-03-01 16:06:58 +0100

    sut

--- js/pokedex.js
+++ js/pokedex.js
@@ -10,48 +10,82 @@ pokeApp.config(['$resourceProvider', function($resourceProvider) {
 pokeApp.controller("Controller", data);
 
   function data($scope,$log,$http){
+
+
     $scope.donnees ={
-    pokemons : []
+    pokemons : [],
+    pokemon : []
+
   };
 
     $scope.$log = $log;
 
     $scope.printPokemon = function(namePokemon){
+
       for (var i = 0; i<$scope.donnees.pokemons.length;i++){
         if ($scope.donnees.pokemons[i].name === namePokemon){
+          $scope.selected = true;
+          console.log($scope.selectedPokemon());
+          $scope.donnees.pokemon=$scope.selectedPokemon();
+          console.log($scope.donnees.pokemon);
           return $scope.donnees.pokemons[i].name;
         }
 
       }
+      $scope.selected = false;
       return "pokemon non trouvé ";
+    }
+
+    $scope.selectedPokemon = function(){
+        var i = 0;
+        while (i < $scope.donnees.pokemons.length){
+          if($scope.donnees.pokemons[i].id === $scope.id){
+
+            return $scope.donnees.pokemons[i].abilities;
+          }
+          i++;
+        }
     }
 
     $scope.changeValue = function($id,$name){
       $scope.id = $id;
       $scope.name = $name;
+
     }
     $scope.reset = function(){
       $scope.id = '';
       $scope.name = '';
+      $scope.selected = false;
+
     }
 
   $http({
       method: 'GET',
       url : "http://pokeapi.co/api/v1/pokedex/"
     }).then(function successCallback(response) {
-      console.log(response.data.objects[0].pokemon);
+      //console.log(response.data.objects[0].pokemon);
        response.data.objects[0].pokemon.forEach(function(poke){
-         var nom=poke.name;
+         var nom = poke.name;
+         var abilities = [];
          $http.get("http://pokeapi.co/"+poke.resource_uri)
             .then(function(response2){
-              $scope.donnees.pokemons.push({"name":nom,"id":response2.data.pkdx_id});
+              response2.data.abilities.forEach(function(ability){
+                abilities.push(ability.name);
+              });
+              $scope.donnees.pokemons.push({"name":nom,"id":response2.data.pkdx_id,"abilities":abilities});
             }
           );
-
-
-
 
         });
         console.log($scope.donnees);
       });
   }
+
+
+  pokeApp.factory('ressourceService', function() {
+    var pikachu = $ressource(
+      POKEAPI+'/api/v1/pokemon/pikachu/',
+      {}
+    );
+
+});
