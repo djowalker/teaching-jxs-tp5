9587b795e22640cef80167793e589dcf0545a164
aodren35 <aodren.letellier@etudiant.univ-rennes1.fr>
2017-04-01 17:14:55 +0200

    Mise au clair
    remise à plat du binz

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,90 +1,90 @@
+// Nom de l'app
 var pokeApp = angular.module('pokedex', ['ngResource']);
 
 // With this you can inject POKEAPI url wherever you want
 pokeApp.constant('POKEAPI', 'http://pokeapi.co');
 
+
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
 
+// Service pour get les informations d'un pokemon
 pokeApp.factory('ressourceService', ['$resource',
     function($resource){
       return $resource('http://pokeapi.co/api/v1/pokemon/:id/', {id:'@id'});
     }]
   );
 
+// Service commun pour les deux controleur avec id et name du pokemon actuel
 pokeApp.factory('serviceCommun', [function(){
-    return {nameCom: "",idCom: ""};
+    return {nameCom: "",idCom: "",selectedCom: false};
   }
 ]);
 
+// Premier contrôleur pour recherche
 pokeApp.controller("Controller", data);
 
-  function data($scope,$log,$http,ressourceService,serviceCommun){
-
+function data($scope,$log,$http,ressourceService,serviceCommun){
     $scope.name = serviceCommun.nameCom;
     $scope.id = serviceCommun.idCom;
-    $scope.$watch(
-        function(){return $scope.id;},
-        function(newValue, oldValue) {
-          if (newValue !== oldValue){
-            serviceCommun.nameCom = $scope.name;
-            serviceCommun.idCom = $scope.id;
-          }
-        }
-    );
-
     $scope.donnees ={
-    pokemons : [],
-    pokemon : {},
-    selected : ""
-  };
+      pokemons : [],
+      pokemonAbi : {},
+      selected : ""
+    };
+    $scope.selected = false ;
 
-    $scope.$log = $log;
-
-    $scope.printPokemon = function(namePokemon){
+    // Print le nom du pokemon en console et le selectionne si il existe e donnée
+    $scope.printPokemon = function(){
       for (var i = 0; i<$scope.donnees.pokemons.length;i++){
-        if ($scope.donnees.pokemons[i].name === namePokemon){
+        if ($scope.donnees.pokemons[i].name === $scope.name){
           $scope.selected = true;
-          $scope.donnees.pokemon=$scope.saveSelectedPokemon(i);
-          return $scope.donnees.pokemons[i].name;
+          $scope.donnees.pokemon=$scope.donnees.pokemons[i];
+          console.log($scope.donnees.pokemon);
+          return;
         }
       }
       $scope.selected = false;
-      return "pokemon non trouvé ";
+      console.log("pokemon non trouvé");
+      return;
     }
 
-    $scope.saveSelectedPokemon = function(i){
-        return $scope.donnees.pokemons[i];
-    }
-
-    $scope.changeValue = function($id,$name){
-      console.log($id,$name);
-      $scope.id = $id;
-      $scope.name = $name;
-      console.log($scope.id,$scope.name);
-
-    }
-    $scope.changeValueBis = function (){
+    // Fonction d'actualisation quand l'utilisateur selectionne un pokemon
+    $scope.changeValue = function (){
       pok = $scope.donnees.selected;
+      $scope.id = pok.slice(0,pok.indexOf(':'));
+      $scope.name = pok.slice(pok.indexOf(':')+1,pok.length);
+      serviceCommun.selectedCom = true;
       console.log(pok);
-       $scope.id = pok.slice(0,pok.indexOf(':'));
-       $scope.name = pok.slice(pok.indexOf(':')+1,pok.length);
-       console.log(serviceCommun);
-       console.log($scope.id,$scope.name);
     }
 
+    // Fonction d'update lorsque l'utilisateur change manuellement les valeurs des input
+    $scope.changeInput = function(a){
+      // input name
+      if (a === 1){
+        $scope.id = "";
+        serviceCommun.selectedCom = false;
+      }
+      // input id
+      else {
+        $scope.name = "";
+        serviceCommun.selectedCom = false;
+      }
+    }
+
+    // Fonction pour reset le form
     $scope.reset = function(){
        $scope.id = '';
        $scope.name = '';
        $scope.selected = false;
     }
 
-  $http({
+    // Requète http pour remplir la liste des pokemons
+    $http({
       method: 'GET',
       url : "http://pokeapi.co/api/v1/pokedex/"
     }).then(function successCallback(response) {
-
        response.data.objects[0].pokemon.forEach(function(poke){
          var nom = poke.name;
          var abilities = [];
@@ -99,36 +99,45 @@ pokeApp.controller("Controller", data);
             }
           );
         });
-
-        console.log($scope.donnees);
+        console.log("donnees",$scope.donnees);
       });
+
+      // $watch pour update le service commun à chaque changement
+      $scope.$watch(
+          function(){return $scope.id;},
+          function(newValue, oldValue) {
+            if (newValue !== oldValue){
+              serviceCommun.nameCom = $scope.name;
+              serviceCommun.idCom = $scope.id;
+            }
+          }
+      );
+      $scope.$watch(
+          function(){return $scope.name;},
+          function(newValue, oldValue) {
+            if (newValue !== oldValue){
+              serviceCommun.nameCom = $scope.name;
+              serviceCommun.idCom = $scope.id;
+            }
+          }
+      );
   }
 
+  // Second contrôleur pour l'affichage des données du pokemon séléctionné
   pokeApp.controller("ControllerRessource", data2);
   function data2($scope,ressourceService,serviceCommun){
-    $scope.$watch(
-        function(){return serviceCommun.nameCom;},
-        function(newValue, oldValue) {
-          if (newValue !== oldValue){
-            $scope.pokemon2.name = serviceCommun.nameCom;
-            $scope.pokemon2.id  = serviceCommun.idCom;
-          }
-          if (newValue === ""){
-            $scope.displayed = false;
-          }
-        }
-    );
     $scope.displayed=false;
     $scope.pokemon2 = {
       name : serviceCommun.nameCom,
       id : serviceCommun.idCom,
       moves : []
     };
-    $scope.displayPokemon = function(){
-      console.log(serviceCommun);
-      console.log($scope.pokemon2);
+
+    $scope.unDisplay = function(){
+      $scope.displayed = false;
+    };
+    $scope.fillMoves = function(){
       var pok = ressourceService.get({id:$scope.pokemon2.id}, function(data){
-          $scope.displayed = true;
 		      // reset de l'array pour chaque nouveau pokemon sélectionné
 		      if($scope.pokemon2.moves.length >1){
 			         $scope.pokemon2.moves.length = 0;
@@ -137,5 +146,25 @@ pokeApp.controller("Controller", data);
 		          $scope.pokemon2.moves.push(move.name);
           });
       });
-    }
+    };
+    $scope.displayPokemon = function(){
+      $scope.displayed = true;
+      $scope.fillMoves();
+      console.log(serviceCommun);
+      console.log($scope.pokemon2);
+
+    };
+    $scope.$watch(
+        function(){return serviceCommun.selectedCom;},
+        function(newValue, oldValue) {
+          if (newValue){
+            $scope.pokemon2.name = serviceCommun.nameCom;
+            $scope.pokemon2.id  = serviceCommun.idCom;
+            $scope.fillMoves();
+          }
+          if (serviceCommun.nameCom === ""){
+            $scope.displayed = false;
+          }
+        }
+    );
   }
